e5fae8886399c2af7990944cf5dbcb86a394e9eb
Lars Gersmann <lars.gersmann@cm4all.com>
2022-11-28 12:48:06 +0100

    wip(@pnpm-kambrium/core): short before working docker deployment

--- packages/npm/core/bin/kambrium-make-docker.mk
+++ packages/npm/core/bin/kambrium-make-docker.mk
@@ -30,21 +30,28 @@ packages/docker/%/: packages/docker/%/build-info ;
 # we utilize file "build-info" to track if the docker image was build/is up to date
 #
 packages/docker/%/build-info: $(filter-out packages/docker/%/build-info,$(wildcard packages/docker$*/* packages/docker$*/**/*)) package.json 
+# read .env file from package if exists 
+> DOT_ENV="packages/docker/$*/.env"; [[ -f $$DOT_ENV ]] && source $$DOT_ENV
 # target depends on root located package.json and every file located in packages/docker/% except build-info 
 > PACKAGE_JSON=$(@D)/package.json
 > PACKAGE_VERSION=$$(jq -r '.version | values' $$PACKAGE_JSON)
 > PACKAGE_AUTHOR="$$(jq -r '.author.name | values' $$PACKAGE_JSON) <$$(jq -r '.author.email | values' $$PACKAGE_JSON)>"
 > PACKAGE_NAME=$$(jq -r '.name | values' $$PACKAGE_JSON | sed -r 's/@//g')
+# if DOCKER_USER is not set take the package scope (example: "@foo/bar" package user is "foo")
+> DOCKER_USER=$${DOCKER_USER:-$${PACKAGE_NAME%/*}}
+# if DOCKER_REPOSITORY is not set take the package repository (example: "@foo/bar" package repository is "bar")
+> DOCKER_REPOSITORY=$${DOCKER_REPOSITORY:-$${PACKAGE_NAME#*/}}
+> DOCKER_IMAGE="$$DOCKER_USER/$$DOCKER_REPOSITORY"
 # @TODO: inject variables from $(@D)/.env (can also be a script!)
 # @TODO: call build script from $$PACKAGE_JSON if defined
 # image labels : see https://github.com/opencontainers/image-spec/blob/main/annotations.md#pre-defined-annotation-keys
 > pnpm run --if-present build $$PACKAGE_NAME
 > docker build \
 > 	--progress=plain \
->		-t $$PACKAGE_NAME:latest \
-> 	-t $$PACKAGE_NAME:$$PACKAGE_VERSION \
+>		-t $$DOCKER_IMAGE:latest \
+> 	-t $$DOCKER_IMAGE:$$PACKAGE_VERSION \
 >		--label "maintainer=$$PACKAGE_AUTHOR" \
-> 	--label "org.opencontainers.image.title=$$PACKAGE_NAME" \
+> 	--label "org.opencontainers.image.title=$$DOCKER_IMAGE" \
 > 	--label "org.opencontainers.image.description=$$(jq -r '.description | values' $$PACKAGE_JSON)" \
 > 	--label "org.opencontainers.image.authors=$$PACKAGE_AUTHOR" \
 >		--label "org.opencontainers.image.source=$$(jq -r -e '.repository.url | values' $$PACKAGE_JSON || jq -r '.repository.url | values' package.json)" \
@@ -54,11 +61,11 @@ packages/docker/%/build-info: $(filter-out packages/docker/%/build-info,$(wildca
 > 	-f $(@D)/Dockerfile .
 # output generated image labels
 > cat << EOF | tee $@
-> $$(docker image inspect $$PACKAGE_NAME:latest | jq '.[0].Config.Labels | values')
+> $$(docker image inspect $$DOCKER_IMAGE:latest | jq '.[0].Config.Labels | values')
 > 
 > ---
 > 
-> $$(docker image ls $$PACKAGE_NAME:$$PACKAGE_VERSION)
+> $$(docker image ls $$DOCKER_IMAGE:$$PACKAGE_VERSION)
 > EOF
 
 #
@@ -79,18 +86,47 @@ docker-push-%: packages/docker/$*/
 > DOT_ENV="packages/docker/$*/.env"; [[ -f $$DOT_ENV ]] && source $$DOT_ENV
 > PACKAGE_JSON=packages/docker/$*/package.json
 > PACKAGE_NAME=$$(jq -r '.name | values' $$PACKAGE_JSON | sed -r 's/@//g')
-# if DOCKER_USER is not set take the package scope (example: "@foo/bar" package scope is "foo")
+# if DOCKER_USER is not set take the package scope (example: "@foo/bar" package user is "foo")
 > DOCKER_USER=$${DOCKER_USER:-$${PACKAGE_NAME%/*}}
+# if DOCKER_REPOSITORY is not set take the package repository (example: "@foo/bar" package repository is "bar")
+> DOCKER_REPOSITORY=$${DOCKER_REPOSITORY:-$${PACKAGE_NAME#*/}}
+> DOCKER_IMAGE="$$DOCKER_USER/$$DOCKER_REPOSITORY"
 # abort if DOCKER_TOKEN is not defined 
 > : $${DOCKER_TOKEN:?"DOCKER_TOKEN environment is required but not given"}
-> echo -n "push docker image $$PACKAGE_NAME using docker user $$DOCKER_USER "
+> echo -n "push docker image $$DOCKER_IMAGE using docker user $$DOCKER_USER "
 > if [[ "$$(jq -r '.private | values' $$PACKAGE_JSON)" != "true" ]]; then  
 > 	PACKAGE_VERSION=$$(jq -r '.version | values' $$PACKAGE_JSON)
 > 	# docker login --username [username] and docker access-token or real password must be initially before push
-> 	echo "$$DOCKER_TOKEN" | docker login --username $$DOCKER_USER --password-stdin $$DOCKER_REGISTRY
-> 	docker push $$PACKAGE_NAME:latest
-> 	docker push $$PACKAGE_NAME:$$PACKAGE_VERSION
+> 	echo "$$DOCKER_TOKEN" | docker login --username "$$DOCKER_USER" --password-stdin $$DOCKER_REGISTRY
+> 	docker push $$DOCKER_IMAGE:latest
+> 	docker push $$DOCKER_IMAGE:$$PACKAGE_VERSION
 >		echo '[done]'
+> 
+>		# if DOCKER_REGISTRY == registry.hub.docker.com : update description and README.md
+>		if [[ "$$DOCKER_REGISTRY" == "registry.hub.docker.com" ]]; then
+> 		: $${DOCKER_PASSWORD:?"DOCKER_PASSWORD environment is required but not given"}
+> 		echo -n "updating description/README.md for docker image $$DOCKER_IMAGE using docker user $$DOCKER_USER "
+# > 			cat ~/my_password.txt | docker login --username foo --password-stdin
+# > 			docker login --username='$(DOCKER_USER)' --password='$(DOCKER_PASS)' $${DOCKER_HOST:-}
+> 		LOGIN_PAYLOAD=$$(printf '{ "username": "%s", "password": "%s" }' "$$DOCKER_USER" "$$DOCKER_PASSWORD")
+> 		JWT_TOKEN=$$(curl -s --show-error  -H "Content-Type: application/json" -X POST -d "$$LOGIN_PAYLOAD" https://hub.docker.com/v2/users/login/ | jq --exit-status -r .token)
+# 		GET : > curl -v -H "Authorization: JWT $${JWT_TOKEN}" "https://hub.docker.com/v2/repositories/$(DOCKER_IMAGE)/"
+> 		DESCRIPTION=$$(docker image inspect --format='' $$DOCKER_IMAGE:latest | jq -r '.[0].Config.Labels["org.opencontainers.image.description"] | values')
+# see https://frontbackend.com/linux/how-to-post-a-json-data-using-curl
+# see https://stackoverflow.com/a/48470227/1554103
+> 		jq -n \
+>   		--arg description "$$(jq -r '.description | values' $$PACKAGE_JSON)" \
+>   		--arg full_description "$$(cat ./docker/README.md ||:)" '{description: $$description, full_description: $$full_description}' \
+>			| curl -s --show-error \
+> 			-H "Content-Type: application/json" \
+>				-H "Authorization: JWT $${JWT_TOKEN}" \
+> 			-X PATCH \
+>				--data-binary @- \
+> 			"https://hub.docker.com/v2/repositories/$$DOCKER_IMAGE/" \
+> 		| jq .
+>			echo '[done]'
+> 	fi
+>
 > else
 > 	echo "[skipped]: package.json is marked as private"
 > fi
