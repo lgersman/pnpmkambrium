a969c475eda9db6ca717ebbd5870622b6f415694
Lars Gersmann <lars.gersmann@cm4all.com>
2022-12-29 15:15:27 +0100

    wip(@pnpmkambrium/core): improved mdbook variable provisioning

--- packages/npm/core/bin/make-docs.mk
+++ packages/npm/core/bin/make-docs.mk
@@ -15,6 +15,8 @@ packages/docs/%/: packages/docs/%/build-info ;
 packages/docs/%/build-info: $(filter-out packages/docs/%/build-info,$(wildcard packages/docs$*/* packages/docs$*/**/*)) package.json 
 # target depends on root located package.json and every file located in packages/docs/% except build-info 
 # > @
+# ensure mdbook image is available
+> @$(call ensure-docker-images-exists, pnpmkambrium/mdbook)
 # set -a causes variables¹ defined from now on to be automatically exported.
 > set -a
 # read .env file from package if exists
@@ -28,17 +30,15 @@ packages/docs/%/build-info: $(filter-out packages/docs/%/build-info,$(wildcard p
 > if jq --exit-status '.scripts | has("build")' $$PACKAGE_JSON 1>/dev/null; then
 > 	echo $(PNPM) -r --filter "$$(jq -r '.name | values' $$PACKAGE_JSON)" run build
 > else
-# >		MDBOOK_BOOK='{"title": "My Awesome Book", "authors": ["Michael-F-Bryan"]}'
-# jq -n --compact-output --arg title "PACKAGE_NAME"       --arg description "PACKAGE_DESCRIPTION" --argjson authors "$(jq --compact-output -j '[.contributors[]? | .name]' packages/docs/gh-pages/package.json)" '{title: $title, description: $description, authors: $authors}'
->		MDBOOK_BOOK=$(jq -n --compact-output \
+>		MDBOOK_BOOK=$$(jq -n --compact-output \
 			--arg title "$$PACKAGE_NAME" \
-      --arg description "$$PACKAGE_DESCRIPTION" \
+     	--arg description "$$PACKAGE_DESCRIPTION" \
 			--argjson authors "$$(jq --compact-output -j '[.contributors[]? | .name]' $$PACKAGE_JSON)" \
 			'{title: $$title, description: $$description, authors: $$authors}' \
 		)
-> 	echo "HUY MDBOOK_BOOK=$$MDBOOK_BOOK"
-# add link to github repo
-> 	docker run --rm -it -e "MDBOOK_BOOK=$$MDBOOK_BOOK" --mount type=bind,source=$$(pwd)/$(@D),target=/data -u $(id -u):$(id -g) pnpmkambrium/mdbook mdbook build
+>		# @TODO: add link to github repo
+>		# @TODO: fallback to author if contributors are not available
+> 	docker run --rm -it -e "MDBOOK_BOOK=$$MDBOOK_BOOK" --mount type=bind,source=$$(pwd)/$(@D),target=/data -u $$(id -u):$$(id -g) pnpmkambrium/mdbook mdbook build
 > fi
 > mkdir -p $(@D)/dist
 # redirecting into the target zip archive frees us from removing an existing archive first
