b6447613f00e78032b99871879a1bdd7ff228e66
Lars Gersmann <lars.gersmann@cm4all.com>
2023-01-03 13:02:50 +0100

    wip(@pnpmkambrium/core): push to gh-pages

--- packages/npm/core/make/make-gh-pages.mk
+++ packages/npm/core/make/make-gh-pages.mk
@@ -25,28 +25,49 @@ gh-pages-push-%: packages/docs/$*/
 > GIT_REPOSITORY_URL=$$(git remote get-url --push $$GIT_REMOTE_REPOSITORY_NAME)
 > # ensure we are not in branch gh-pages yet
 > [[ $$(git rev-parse --abbrev-ref HEAD) == 'gh-pages' ]] && 1>&2 echo "cannot push gh-pages : current branch is gh-pages. switch to another branch and try again." && exit 1
-> # ensure there are no uncommitted changes in current branch
+# > # ensure there are no uncommitted changes in current branch
 # > [[ -z "$(git status --porcelain)" ]] && 1>&2 echo "cannot push gh-pages : current branch contains uncommited changes. commit changes and try again." && exit 1
 > # check if branch gh-pages exists on remote
 > if ! git ls-remote --exit-code --heads "$$GIT_REPOSITORY_URL" gh-pages; then
-> 	PAST_BRANCH=$$(git rev-parse --abbrev-ref HEAD)
->		# copy sub package build directory to a tempory directory
->		TMP_DIR=$$(mktemp -d --suffix ".$@")
->		cp -r packages/docs/$*/build $$TMP_DIR/
->		# create or switch to local branch gh-pages and pull latest from remote gh-pages
-> 	git switch --quiet -C gh-pages || git pull $$GIT_REMOTE_REPOSITORY_NAME
->		# delete anything in gh-stages branch
+
+# git clone /home/lgersman/workspace/pnpmkambrium
+# cd pnpmkambrium/
+# git switch --orphan gh-pages
+# -t, --track           set upstream info for new branch
+# git checkout develop .gitignore
+# git status
+# git reset --hard
+# git commit --allow-empty -m "Initializing gh-pages branch"
+# git log
+# git status
+# ls -la
+# git status
+# git add .
+# git commit
+# git status
+# git branch
+# git push
+# git push --set-upstream origin gh-pages
+# git branch
+# xed .gitignore
+
+# > 	PAST_BRANCH=$$(git rev-parse --abbrev-ref HEAD)
+# >		# copy sub package build directory to a tempory directory
+# >		TMP_DIR=$$(mktemp -d --suffix ".$@")
+# >		cp -r packages/docs/$*/build $$TMP_DIR/
+# >		# create or switch to local branch gh-pages and pull latest from remote gh-pages
+# > 	git switch --quiet -C gh-pages || git pull $$GIT_REMOTE_REPOSITORY_NAME
+# >		# delete anything in gh-stages branch
 # >		rm -fr * .!(|.|git)
->		# copy build folder into branch gh-stages
+# >		# copy build folder into branch gh-stages
 # >		mv $$TMP_DIR/ .
 # see https://gist.github.com/cobyism/4730490#gistcomment-1374989
 # >  and I won't have to delete the remote branch every time, which is unnecessary.
 # > https://stackoverflow.com/questions/52087783/git-push-to-gh-pages-updates-were-rejected
->		
->		# push branch to remote 
+# >		# push branch to remote 
 # > 	git push $$GIT_REMOTE_REPOSITORY_NAME gh-pages:gh-pages
-> 	# switch back to previous branch
-> 	git switch $$PAST_BRANCH						
+# > 	# switch back to previous branch
+# > 	git switch $$PAST_BRANCH						
 > fi
 # > GITHUB_REPO_NAME="$$(\
 # 		jq --exit-status -r '.repository.url | select(.!=null)' $$PACKAGE_JSON || \
