9384ac3ab7e3e0bb0da583f793b9984b6b6ab94e
Lars Gersmann <lars.gersmann@gmail.com>
2023-01-10 06:44:35 +0100

    fix(@pnpmkambrium/core): improved target github-details-push

--- packages/npm/core/make/make-github.mk
+++ packages/npm/core/make/make-github.mk
@@ -18,11 +18,11 @@
 # 	- GITHUB_REPO_TOPICS (optional,default=value of root package.json property 'keys')
 # 	- GITHUB_REPO_HOMEPAGE (optional,default=value of root package.json property 'homepage')
 #
-# test using `GITHUB_TOKEN="foo" GITHUB_OWNER="bar" make --silent github-update-docs`
-.PHONY: github-update-docs
+# test using `GITHUB_TOKEN="foo" GITHUB_OWNER="bar" make --silent github-details-push`
+.PHONY: github-details-push
 #HELP: * update github repo documentation.\n\texample: 'GIT_REMOTE_REPOSITORY_NAME=my-origin make gh-pages-push-foo' to push 'build' folder contents of docs package 'packages/docs/foo' to git remote repo with name 'my-origin'
 # it's tricky - target will depend on 'packages/docs/gh-pages/' if packages/docs/gh-pages/package.json exists and property .private is true
-github-update-docs: $(shell jq --exit-status '.private? | not' packages/docs/gh-pages/package.json >/dev/null 2>&1 && echo 'packages/docs/gh-pages/build-info' || echo "")
+github-details-push: $(shell jq --exit-status '.private? | not' packages/docs/gh-pages/package.json >/dev/null 2>&1 && echo 'packages/docs/gh-pages/build-info' || echo "")
 > # https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#user-agent-required
 > # https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28
 > # https://gist.github.com/btoone/2288960
@@ -32,28 +32,27 @@ github-update-docs: $(shell jq --exit-status '.private? | not' packages/docs/gh-
 > : $${GITHUB_OWNER:?"GITHUB_OWNER environment is required but not given"}
 > # read .env file from package if exists
 > DOT_ENV="packages/docs/$*/.env"; [[ -f $$DOT_ENV ]] && source $$DOT_ENV
-> GITHUB_REPO=$${GITHUB_REPO:-$$(jq -r '.name | values' package.json)}
-> echo "GITHUB_REPO=$$GITHUB_REPO"
-> echo "GITHUB_OWNER=$$(printenv GITHUB_OWNER)"
+> PACKAGE_NAME=$$(jq -r '.name | values' package.json)
+> GITHUB_REPO=$${GITHUB_REPO:-$$PACKAGE_NAME}
 > GITHUB_REPO_DESCRIPTION=$${GITHUB_REPO_DESCRIPTION:-$$(jq --exit-status -r '.description | values' package.json)}
-> echo "GITHUB_REPO_DESCRIPTION=$$GITHUB_REPO_DESCRIPTION"
 > GITHUB_REPO_TOPICS=$${GITHUB_REPO_TOPICS:-$$(jq --exit-status '.keywords' package.json || echo '[]')}
-> echo "GITHUB_REPO_TOPICS=$$GITHUB_REPO_TOPICS"
 > GITHUB_REPO_HOMEPAGE=$${GITHUB_REPO_HOMEPAGE:-$$(jq -r --exit-status '.homepage | values' package.json)}
-> echo "GITHUB_REPO_HOMEPAGE=$$GITHUB_REPO_HOMEPAGE"
+> echo "push '$$PACKAGE_NAME' github repo details"
 # if sub package 'gh-pages' exists
 >	if [[ "$^" != '' ]]; then
 >		echo "dependencies are '$^'"
 > else 
 >		echo "sub package gh-pages doenst exist"
 > fi
-> # update description and tags
-> jq -n \
+> # update description and homepage
+> DATA=`jq -n \
 > 	--arg description "$$GITHUB_REPO_DESCRIPTION" \
 >   --argjson topics "$$GITHUB_REPO_TOPICS" \
 >   --arg homepage "$$GITHUB_REPO_HOMEPAGE" \
 > 	'{description: $$description, homepage: $$homepage, topics: $$topics}' \
->	| curl -s --show-error \
+> `
+> jq . <(echo $$DATA)
+>	echo $$DATA | curl -s --show-error \
 >		--fail \
 >  	-X PATCH \
 > 	-H "Accept: application/vnd.github+json" \
@@ -61,4 +60,4 @@ github-update-docs: $(shell jq --exit-status '.private? | not' packages/docs/gh-
 >  	https://api.github.com/repos/$${GITHUB_OWNER}/$${GITHUB_REPO} \
 > 	--data-binary @- \
 > 	| jq .
-
+> echo '[done]'
