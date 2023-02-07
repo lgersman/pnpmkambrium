90d072852ad8a5e35ef18c9d7a7dbfd94cfd5130
Lars Gersmann <lars.gersmann@gmail.com>
2023-02-07 18:25:34 +0100

    wip(@pnpmkambrium/core): rewrote help target to be a separate bash script function

--- packages/npm/core/make/make-bash-functions.sh
+++ packages/npm/core/make/make-bash-functions.sh
@@ -16,3 +16,63 @@ function kambrium:jq:first_non_empty_array() {
 
   echo '[]'
 }
+
+#
+# computes help for the Makefile
+# 
+# required environment variables:
+#   VERBOSE(default='' is disabled)   dumps more output on stderr 
+#   FORMAT(json/text,default=text)    output format. text means output send to desktop, json provides help in json format for further processing
+#
+# returns the computed help information
+#
+function kambrium:help() {
+  declare -A HELP_TOPICS=()
+  IFS='\n'
+  # pipe all read makefiles into read loop
+  while read line; do
+    # if help heredoc marker matches /#\s<<([\w\:\-\_]+)/ current line 
+    if [[ "$line" =~ ^#[[:blank:]]\<\<([[:print:]]+)$ ]]; then 
+      HEREDOC_KEY="${BASH_REMATCH[1]}"
+      declare -a HEREDOC_BODY=()
+      # read lines starting with '# ' until a line containing just the heredoc token comes in 
+      while read line; do
+        if [[ "$line" =~ ^#[[:blank:]]$HEREDOC_KEY$ ]]; then
+          # join lines separated by \n
+          HEREDOC_BODY=$(printf "\n%s" "${HEREDOC_BODY[@]}")
+          # strip leading \n
+          HEREDOC_BODY=${HEREDOC_BODY:1}
+          if [[ "$HEREDOC_BODY" == '' ]]; then
+            [[ "$VERBOSE" != '' ]] && echo "[skipped] Help HereDoc(='$HEREDOC_KEY') : help body is empty" >&2
+          else 
+            [[ "$VERBOSE" != '' ]] && echo "'$HEREDOC_KEY'='$HEREDOC_BODY'" >&2
+            # read while we match a make target
+            while read line; do
+              # line="$(printf '.PHONY bar\t  foo/x.txt') %.cpp :"; [[ "$line" =~ ^((([A-Za-z0-9_/.\%]+)[[:blank:]]*)+): ]] && [[ "${line::1}" != '.' ]]  && echo "matched '${BASH_REMATCH[0]}'"
+              if [[ "$line" =~ ^((([A-Za-z0-9_/.\%]+)[[:blank:]]*)+): ]] && [[ "${line::1}" != '.' ]]; then
+                HELP_TOPICS["${BASH_REMATCH[1]}"]="$HEREDOC_BODY"
+                break
+              fi 
+            done
+          fi
+          break
+        elif [[ "$line" =~ ^#[[:blank:]](([[:print:]]|[[:space:]])+)$ ]]; then
+          HEREDOC_BODY+=(${BASH_REMATCH[1]})
+        else
+          [[ "$VERBOSE" != '' ]] && echo "[skipped] Help HereDoc(='$HEREDOC_KEY') : line '$line' does not match help line prefix(='# ') nor HereDoc end marker(='$HEREDOC_KEY')" >&2
+          break
+        fi
+      done
+    fi
+  done
+
+  if [[ "${FORMAT:-}" == 'json' ]]; then
+    echo "json output selected"
+  else
+    echo "############## FORMAT text"
+    
+    for TARGET in "${!HELP_TOPICS[@]}"; do
+      printf "${TARGET}:\n${HELP_TOPICS[$TARGET]}\n\n" | cat
+    done    
+  fi
+}
--- packages/npm/core/make/make-help.mk
+++ packages/npm/core/make/make-help.mk
@@ -23,55 +23,10 @@ bar:
 #
 .PHONY: foo
 foo:
->	declare -A HELP_TOPICS=()
-> IFS='\n'
->	# pipe all read makefiles into read loop
->	while read line; do
->		# if help heredoc marker matches /#\s<<([\w\:\-\_]+)/ current line 
->		if [[ "$$line" =~ ^#[[:blank:]]\<\<([[:print:]]+)$$ ]]; then 
->			HEREDOC_KEY="$${BASH_REMATCH[1]}"
->			declare -a HEREDOC_BODY=()
->			# read lines starting with '# ' until a line containing just the heredoc token comes in 
->			while read line; do
->				if [[ "$$line" =~ ^#[[:blank:]]$$HEREDOC_KEY$$ ]]; then
->					# join lines separated by \n
->					HEREDOC_BODY=$$(printf "\n%s" "$${HEREDOC_BODY[@]}")
->					# strip leading \n
->					HEREDOC_BODY=$${HEREDOC_BODY:1}
->					if [[ "$$HEREDOC_BODY" == '' ]]; then
->						echo "[skipped] Help HereDoc(='$$HEREDOC_KEY') : help body is empty" >&2
->					else 
->						echo "'$$HEREDOC_KEY'='$$HEREDOC_BODY'" >&2
-> 					while read line; do
-# line="$(printf '.PHONY bar\t  foo/x.txt') %.cpp :"; [[ "$line" =~ ^((([A-Za-z0-9_/.\%]+)[[:blank:]]*)+): ]] && [[ "${line::1}" != '.' ]]  && echo "matched '${BASH_REMATCH[0]}'"
-> 						if [[ "$$line" =~ ^((([A-Za-z0-9_/.\%]+)[[:blank:]]*)+): ]] && [[ "$${line::1}" != '.' ]]; then
-> 							HELP_TOPICS["$${BASH_REMATCH[1]}"]="$$HEREDOC_BODY"
-> 							break
-> 						fi 
-> 					done
->					fi
->					break
->				elif [[ "$$line" =~ ^#[[:blank:]](([[:print:]]|[[:space:]])+)$$ ]]; then
->					HEREDOC_BODY+=($${BASH_REMATCH[1]})
->				else
->					echo "[skipped] Help HereDoc(='$$HEREDOC_KEY') : line '$$line' does not match help line prefix(='# ') nor HereDoc end marker(='$$HEREDOC_KEY')" >&2
->					break
->				fi
->			done
->		fi
->	done < <(cat $(MAKEFILE_LIST))
-> 
-> if  [[ "$${HELP_MODE:-}" == '' ]]; then
->		echo "############## HELP_MODE is undefined"
->		
->		for TARGET in "$${!HELP_TOPICS[@]}"; do
-> 		printf "$${TARGET}:\n$${HELP_TOPICS[$$TARGET]}\n\n" | cat
-> 	done
->		
-> else
-> 	echo "HELP_MODE=$${HELP_MODE:-}"
-> fi
-> # convert HELP associative bash array into json
+> # import kambrium bash function library
+> . "$(KAMBRIUM_MAKEFILE_DIR)/make-bash-functions.sh"
+> help=$$( VERBOSE=$${VERBOSE:-}; FORMAT=$${FORMAT:-text}; kambrium:help < <(cat $(MAKEFILE_LIST)) )
+> echo -e "$$help"
 
 #
 # <<HELP
@@ -92,3 +47,4 @@ xxxx:
 #
 .PHONY: yyyy
 yyyy:
+
