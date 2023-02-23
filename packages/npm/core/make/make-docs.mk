3af0abe352828ce94144da86459d63131700dfa9
Lars Gersmann <lars.gersmann@gmail.com>
2023-02-23 11:33:31 +0100

    docs(@pnpmkambrium/core): updated docs for make target 'dev-docs-%'

--- packages/npm/core/make/make-docs.mk
+++ packages/npm/core/make/make-docs.mk
@@ -114,9 +114,11 @@ packages/docs/%/build-info: $(KAMBRIUM_SUB_PACKAGE_BUILD_INFO_DEPS)
 # HELP<<EOF
 # start dev server for a docs sub package in `packages/docs/`
 # 
+# please note that this target __MUST__ be called with make option `-b`.
+# 
 # using this target enables you to edit a docs sub package and preview it in the browser at your fingertips
 #
-# example: `make dev-docs-foo`
+# example: `make -B dev-docs-foo`
 #
 #      will start the dev server for docs sub package `foo`. 
 #      Every change in `packages/docs/foo` will result in rebuild/reloading the docs in the browser.
