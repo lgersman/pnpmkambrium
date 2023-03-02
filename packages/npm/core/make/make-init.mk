15d1e8299fd9170e6639b4b1a010c6bd286cfdb6
Lars Gersmann <lars.gersmann@gmail.com>
2023-03-02 07:51:34 +0100

    wip(@pnpmkambrium/core): fighting with make init

--- packages/npm/core/make/make-init.mk
+++ packages/npm/core/make/make-init.mk
@@ -1,8 +1,8 @@
 #
 # HELP<<EOF
-# initialize a pnpmkambrium workspace and (re)create all files / settings for a pnpmkambrium workspace  
+# initialize a pnpmkambrium workspace and (re)create required files / settings for a pnpmkambrium workspace  
 #
-# this target is automagically called when the @pnpmkambrium/core package gets installed. 
+# this target is automagically called when the @pnpmkambrium/core package gets installed by node package manager. 
 # 
 # you can call it even manually to force (re)creation of all files/settings required by pnpmkambrium
 #
@@ -17,9 +17,10 @@
 # 
 .PHONY: init
 init: 
+> 	set -x
 > # initialize our gitignore list in .git/info/excludes 
 > # see https://stackoverflow.com/a/45018435/1554103 
-> GIT_EXCLUDE_TEMPLATE=node_modules/@pnpmkambrium/core/presets/default/.gitignore
+> GIT_EXCLUDE_TEMPLATE=$$(realpath --relative-to=$$(pwd) node_modules/@pnpmkambrium/core/presets/default/.gitignore)
 > GIT_EXCLUDE=.git/info/exclude
 > if [[ -L $$GIT_EXCLUDE ]] && diff $$GIT_EXCLUDE $$GIT_EXCLUDE_TEMPLATE; then
 > 	# if GIT_EXCLUDE exists and is a symlink (-L) and points exact same content as GIT_EXCLUDE_TEMPLATE
@@ -27,5 +28,5 @@ init:
 > else
 > 	rm -f $$GIT_EXCLUDE
 > 	# create a symlink GIT_EXCLUDE pointing to GIT_EXCLUDE_TEMPLATE
-> 	printf "[done] create local git ignore file : $$(ln -s -v $$GIT_EXCLUDE_TEMPLATE $$GIT_EXCLUDE)\n"
+> 	printf "[done] create local git ignore file : $$(ln -s $$GIT_EXCLUDE_TEMPLATE $$GIT_EXCLUDE)\n"
 > fi
