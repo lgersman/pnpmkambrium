7ca513f13d6e250ebde881369f6ae31120cda63c
Lars Gersmann <lars.gersmann@gmail.com>
2023-03-05 17:52:17 +0100

    refactor(@pnpmkambrium/core): get rid of .gitignore file by using git config

--- packages/npm/core/make/make-init.mk
+++ packages/npm/core/make/make-init.mk
@@ -13,27 +13,35 @@
 #
 #  example: `make init` 
 #
+# 
+# see [git.core documentation](https://github.com/git/git/blob/master/Documentation/config/core.txt)
 # EOF
 # 
 .PHONY: init
-init: 
+init:
 > KAMBRIUM_CORE_PATH=$$(realpath --relative-to=$$(pwd) node_modules/@pnpmkambrium/core)
 > # configure git to use our git hooks
 > git config core.hookspath "$${KAMBRIUM_CORE_PATH}/presets/default/.githooks"
 > 
-> # configure git to use true symlinks (https://github.com/git/git/blob/master/Documentation/config/core.txt)
+> # configure git to use true symlinks
 > git config core.symlinks true
 > 
-> # initialize our gitignore list in .git/info/excludes 
-> # see https://stackoverflow.com/a/45018435/1554103 
-> {
->   GIT_EXCLUDE_TEMPLATE="$${KAMBRIUM_CORE_PATH}/presets/default/.gitignore"
->   GIT_EXCLUDE=.git/info/exclude
->   if [[ ! -L $$GIT_EXCLUDE ]] || ! cmp -s -- $$GIT_EXCLUDE $$GIT_EXCLUDE_TEMPLATE; then
->     # if (GIT_EXCLUDE not exists or not is a symlink (-L) or not points exact same content as GIT_EXCLUDE_TEMPLATE)
->     # create a symlink GIT_EXCLUDE pointing to GIT_EXCLUDE_TEMPLATE
->     #  (note that we use ln -r to automatically convert the target path to ../../$$GIT_EXCLUDE_TEMPLATE)
->     # the ln -f option will force overwriting GIT_EXCLUDE even if its a existing file/link
->     printf "[done] link local git exclude file to kambrium git exclude list : $$(ln -s -r -f -v $$GIT_EXCLUDE_TEMPLATE $$GIT_EXCLUDE)\n"
->   fi
-> }
+> # configure our gitignore list (${KAMBRIUM_CORE_PATH}/presets/default/.gitignore) via git config 
+> # (best solution so far compared to linking to .gitignore or .git/info/exclude)
+> git config core.excludesFile "$${KAMBRIUM_CORE_PATH}/presets/default/.gitignore"
+# > {
+# >   GIT_EXCLUDE_TEMPLATE="$${KAMBRIUM_CORE_PATH}/presets/default/.gitignore"
+# >   GIT_EXCLUDE=.git/info/exclude
+# >   if [[ ! -L $$GIT_EXCLUDE ]] || ! cmp -s -- $$GIT_EXCLUDE $$GIT_EXCLUDE_TEMPLATE; then
+# >     # if (GIT_EXCLUDE not exists or not is a symlink (-L) or not points exact same content as GIT_EXCLUDE_TEMPLATE)
+# >     # create a symlink GIT_EXCLUDE pointing to GIT_EXCLUDE_TEMPLATE
+# >     #  (note that we use ln -r to automatically convert the target path to ../../$$GIT_EXCLUDE_TEMPLATE)
+# >     # the ln -f option will force overwriting GIT_EXCLUDE even if its a existing file/link
+# >     printf "[done] link local git exclude file to kambrium git exclude list : $$(ln -s -r -f -v $$GIT_EXCLUDE_TEMPLATE $$GIT_EXCLUDE)\n"
+# >   fi
+# > }
+> 
+> # tell git to treat all text files "to have `LF` line endings
+> # instead of a .gitattributes file we can simply set git config option pointing to our git attribution 
+> # (see details here : https://github.com/git/git/blob/master/Documentation/config/core.txt)
+> git config core.attributesFile "$${KAMBRIUM_CORE_PATH}/presets/default/.gitattributes"
