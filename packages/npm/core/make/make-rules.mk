092406eb9e550a04d25650858ab22b8cf2b97b5f
Lars Gersmann <lars.gersmann@gmail.com>
2023-03-15 17:31:32 +0100

    refactor(@pnpmkambrium/core): improved guard-env-% make rule

--- packages/npm/core/make/make-rules.mk
+++ packages/npm/core/make/make-rules.mk
@@ -8,8 +8,7 @@
 #
 # see https://stackoverflow.com/a/7367903/1554103
 guard-env-%:
-> @
-> if [ "${${*}}" = "" ]; then \
->     echo "Environment variable $* not set"; \
->     exit 1; \
+> if [[ "${${*}}" == "" ]]; then
+>   echo "Environment variable $* not set" >&2
+>   exit 1
 > fi
