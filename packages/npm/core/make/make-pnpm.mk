bc64e35532428adfa89349ca4e8d67cd09c5d3bb
Lars Gersmann <lars.gersmann@cm4all.com>
2023-04-03 12:42:34 +0200

    fix(@pnpmkambrium/core): removed @ predicate

--- packages/npm/core/make/make-pnpm.mk
+++ packages/npm/core/make/make-pnpm.mk
@@ -3,11 +3,11 @@
 # ensure pnpm is available
 ifeq (,$(shell command -v pnpm))
   define PNPM_NOT_FOUND
-pnpm is not installed or not in PATH. 
+pnpm is not installed or not in PATH.
 Install it using "wget -qO- 'https://get.pnpm.io/install.sh' | sh -"
-(windows : 'iwr https://get.pnpm.io/install.ps1 -useb | iex') 
+(windows : 'iwr https://get.pnpm.io/install.ps1 -useb | iex')
 
-See more here : https://docs.npmjs.com/getting-started/installing-node 
+See more here : https://docs.npmjs.com/getting-started/installing-node
   endef
   $(error $(PNPM_NOT_FOUND))
 else
@@ -18,8 +18,8 @@ endif
 # (required by pnpm)
 ifeq (,$(shell command -v node))
   define NODEJS_NOT_FOUND
-node is not installed or not in PATH. 
-See more here : https://nodejs.org/en/download/ 
+node is not installed or not in PATH.
+See more here : https://nodejs.org/en/download/
   endef
   $(error $(NODEJS_NOT_FOUND))
 endif
@@ -31,17 +31,17 @@ NODE_VERSION != sed -n '/^use-node-version=/ {s///p;q;}' .npmrc
 # path to node binary configured in .npmrc
 NODE := $(HOME)/.local/share/pnpm/nodejs/$(NODE_VERSION)/bin/node
 
-# this target triggers pnpm to download/install the required nodejs if not yet available 
+# this target triggers pnpm to download/install the required nodejs if not yet available
 $(NODE):
 # > @$(PNPM) exec node --version 1&>/dev/null
 # > touch -m $@
 
-pnpm-lock.yaml: package.json 
->  $(PNPM) install --lockfile-only
-> @touch -m pnpm-lock.yaml
+pnpm-lock.yaml: package.json
+> $(PNPM) install --lockfile-only
+> touch -m pnpm-lock.yaml
 
-node_modules/: pnpm-lock.yaml 
+node_modules/: pnpm-lock.yaml
 # pnpm bug: "pnpm use env ..." is actually not needed but postinall npx calls fails
 > $(PNPM) env use --global $(NODE_VERSION)
->  $(PNPM) install --frozen-lockfile
-> @touch -m node_modules
+> $(PNPM) install --frozen-lockfile
+> touch -m node_modules
