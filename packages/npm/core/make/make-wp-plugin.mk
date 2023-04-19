4da1eabdaeeb7b6a1450f5cc35e2a1badb4fcd86
Lars Gersmann <lars.gersmann@cm4all.com>
2023-04-19 12:27:56 +0200

    feat(@pnpmkambrium/core): added "pre-build" and "post-build" sub package script support

--- packages/npm/core/make/make-wp-plugin.mk
+++ packages/npm/core/make/make-wp-plugin.mk
@@ -10,9 +10,9 @@ packages/wp-plugin/: $(KAMBRIUM_SUB_PACKAGE_FLAVOR_DEPS) ;
 # build and zip outdated wordpress plugin package by name.
 #
 # plugin metadata like author/description/version will be taken from sub package file `package.json`
-# 
-# example: `make packages/wp-plugin/foo/` 
-# 
+#
+# example: `make packages/wp-plugin/foo/`
+#
 #   will build the wordpress plugin sub package `packages/wp-plugin/foo`
 # EOF
 packages/wp-plugin/%/: $(KAMBRIUM_SUB_PACKAGE_DEPS) ;
@@ -25,65 +25,70 @@ packages/wp-plugin/%/build-info: $(KAMBRIUM_SUB_PACKAGE_BUILD_INFO_DEPS)
 > kambrium:load_env $(@D)
 > PACKAGE_JSON=$(@D)/package.json
 > PACKAGE_VERSION=$$(jq -r '.version | values' $$PACKAGE_JSON)
-> PACKAGE_AUTHOR="$$(kambrium:author_name $$PACKAGE_JSON) <$$(kambrium:author_email $$PACKAGE_JSON)>"
-> PACKAGE_NAME=$$(jq -r '.name | values' $$PACKAGE_JSON | sed -r 's/@//g')
 > rm -rf $(@D)/{dist,build}
-> mkdir -p $(@D)/{dist,build}
+> $(PNPM) -r --filter "$$(jq -r '.name | values' $$PACKAGE_JSON)" --if-present run pre-build
+> if jq --exit-status '.scripts | has("build")' $$PACKAGE_JSON >/dev/null; then
+>   $(PNPM)-r --filter "$$(jq -r '.name | values' $$PACKAGE_JSON)" run build
+> else
 > # @TODO: build wordpress plugin
 > # - build js/css in src/ (take package.json src/entry info into account)
 > # - generate/update i18n resources pot/mo/po
 > # - update plugin.php readme.txt or use readme.txt.template => readme.txt mechnic
 > # - resize/generate wordpress.org plugin images
-> # - update plugin.php metadata 
+> # - update plugin.php metadata
 > # - transpile build/php sources down to 7.4. if needed (lookup required php version from plugin.php)
-> # how do we store the original plugin.zip and the transpiled plugin within build/ folder ?  
-> $(PNPM) -r --filter "$$(jq -r '.name | values' $$PACKAGE_JSON)" run build
-> # @TODO: zip build/* folder contents 
+> # how do we store the original plugin.zip and the transpiled plugin within build/ folder ?
+> fi
+> $(PNPM) -r --filter "$$(jq -r '.name | values' $$PACKAGE_JSON)" --if-present run post-build
+> [[ -d '$(@D)/build' ]] || (echo "don't unable to archive build directory(='$(@D)/build') : directory does not exist" >&2 && false)
+> find $(@D)/build -name "*.kambrium-template" -exec rm -v -- {} \;
+> mkdir -p $(@D)/dist
+> # @TODO: zip build/* folder contents
 # > (cd $(@D) && $(PNPM) pack --pack-destination ./dist >/dev/null)
 # > ARCHIVE_NAME="$$(basename $<)-v$${PLUGIN_VERSION}.zip"
-# > cd $< && zip -qq -r -o ../$$ARCHIVE_NAME * 
-> # output zip archives 
+# > cd $< && zip -qq -r -o ../$$ARCHIVE_NAME *
+> # output zip archives
 > cat << EOF | tee $@
-> $$(cd $(@D)/dist && ls -1shS *.zip) 
-> 
+> $$(cd $(@D)/dist && ls -1shS *.zip)
+>
 > $$(echo -n "---")
-> 
+>
 > @TODO: list archive contents
 > (tar -ztf $(@D)/dist/*.zip | sort)
 > EOF
 
 # HELP<<EOF
 # push wordpress plugin to wordpress.org
-# 
+#
 # see supported environment variables on target `wp-plugin-push-%`
 # EOF
 .PHONY: wp-plugin-push
 wp-plugin-push: $(foreach PACKAGE, $(shell find packages/wp-plugin/ -mindepth 1 -maxdepth 1 -type d -printf "%f " 2>/dev/null ||:), $(addprefix wp-plugin-push-, $(PACKAGE))) ;
 
 # HELP<<EOF
-# push wordpress plugin to wordpress.org 
-# 
-# target will also update 
+# push wordpress plugin to wordpress.org
+#
+# target will also update
 #
 #   - the readme.txt description/version/author using the `description` property of sub package file `package.json`
-#   - the wordpress images  
-# 
-# at wordpress.org using 
-# 
-# supported variables are: 
-#   - `WORDPRESS_TOKEN` (required) the wordpress.org account password 
-#   - `WORDPRESS_USER` (optional,default=sub package scope without `@`) the wordpress.org identity/username.  
-#   - `WORDPRESS_PLUGIN` (optional,default=sub package name part after `/`) 
+#   - the wordpress images
+#
+# at wordpress.org using
+#
+# supported variables are:
+#   - `WORDPRESS_TOKEN` (required) the wordpress.org account password
+#   - `WORDPRESS_USER` (optional,default=sub package scope without `@`) the wordpress.org identity/username.
+#   - `WORDPRESS_PLUGIN` (optional,default=sub package name part after `/`)
 #
 # environment variables can be provided using:
 #   - make variables provided at commandline
 #   - `.env` file from sub package
 #   - `.env` file from monorepo root
 #   - environment
-# 
+#
 # example: `make wp-plugin-push-foo WORDPRESS_USER=foo WORDPRESS_TOKEN=foobar`
-# 
-#    will build (if outdated) the wordpress plugins and push it ot wordpress.org 
+#
+#    will build (if outdated) the wordpress plugins and push it ot wordpress.org
 # EOF
 .PHONY: wp-plugin-push-%
 wp-plugin-push-%: packages/wp-plugin/$$*/
@@ -95,10 +100,10 @@ wp-plugin-push-%: packages/wp-plugin/$$*/
 > WORDPRESS_USER=$${WORDPRESS_USER:-$${PACKAGE_NAME%/*}}
 # if WORDPRESS_PLUGIN is not set take the package repository (example: "@foo/bar" wordpress plugin is "bar")
 > WORDPRESS_PLUGIN=$${WORDPRESS_PLUGIN:-$${PACKAGE_NAME#*/}}
-> abort if WORDPRESS_TOKEN is not defined 
+> abort if WORDPRESS_TOKEN is not defined
 > : $${WORDPRESS_TOKEN:?"WORDPRESS_TOKEN environment is required but not given"}
 > echo "push wordpress plugin $$WORDPRESS_PLUGIN to wordpress.org using user $$WORDPRESS_USER"
-> if [[ "$$(jq -r '.private | values' $$PACKAGE_JSON)" != "true" ]]; then  
+> if [[ "$$(jq -r '.private | values' $$PACKAGE_JSON)" != "true" ]]; then
 >   PACKAGE_VERSION=$$(jq -r '.version | values' $$PACKAGE_JSON)
 >   # @TODO: push plugin to wordpress.org
 >   echo '[done]'
