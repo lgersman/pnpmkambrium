7dd3641fd7d1a5479914dd6572a1a846c5ada3f1
Lars Gersmann <lars.gersmann@gmail.com>
2023-11-03 17:23:40 +0100

    style(@pnpmkambrium/core): fixed formatting

--- packages/npm/core/make/make-functions.mk
+++ packages/npm/core/make/make-functions.mk
@@ -1,20 +1,20 @@
-# contains generic make functions 
+# contains generic make functions
 
 # function for testing the existence of a list of commands
 #
-# example usage : 
-# mytarget: 
+# example usage :
+# mytarget:
 # # ensure commands  ls, dd, dudu and lxop exists
 # > $(call ensure-commands-exists, ls dd dudu lxop)
 #
-# see see https://www.gnu.org/software/make/manual/make.html#Call-Function and https://www.gnu.org/software/make/manual/make.html#Canned-Recipes
+# see https://www.gnu.org/software/make/manual/make.html#Call-Function and https://www.gnu.org/software/make/manual/make.html#Canned-Recipes
 ensure-commands-exists = $(foreach command,$1,\
   $(if $(shell command -v $(command)),some string,$(error "Could not find executable '$(command)' - please install '$(command)'")))
 
 # function for testing the existence of a list of docker images
 #
-# example usage : 
-# mytarget: 
+# example usage :
+# mytarget:
 # > # ensure images  foo/bar, fedora:latest exists
 # > $(call ensure-docker-images-exists, foo/bar fedora:latest)
 #
@@ -25,18 +25,18 @@ define ensure-docker-images-exists
         make_target="$$(realpath --relative-to=$(CURDIR) $$(pnpm --filter="@$(image)" exec pwd))/"
         echo "Image '@$(image)' not available : It's available as monorepo sub package(path='$$make_target') - build it and try again." >&2
         exit -1
-      else 
+      else
         docker pull $$DOCKER_FLAGS $(DOCKER_FLAGS) "$(image)" >/dev/null 2>&1 || echo "Image '@$(image)' not available : could not download image from docker hub" >&2
         exit -1
       fi
     fi
-  ) 
+  )
 endef
 
 #
-# dump make variables 
-# example usage : 
-# mytarget: 
+# dump make variables
+# example usage :
+# mytarget:
 # > $(call kambrium_dump_vars)
 #
 define kambrium_dump_vars
