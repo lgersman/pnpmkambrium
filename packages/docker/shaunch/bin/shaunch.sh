7a42cba765c36ff01cde189308b5f2680a9b8a58
Lars Gersmann <lars.gersmann@gmail.com>
2023-11-09 07:52:43 +0100

    fix(@pnpmkambrium/shaunch): shaunch supports copy/paste

--- packages/docker/shaunch/bin/shaunch.sh
+++ packages/docker/shaunch/bin/shaunch.sh
@@ -2,10 +2,10 @@
 
 # Desc: shows a list of commands and their markdown rendered documentation
 #
-# Usage: execute shaunch.sh in the root of a monorepo 
+# Usage: execute shaunch.sh in the root of a monorepo
 #
-# script supports customization using @TODO: 
-# Example: 
+# script supports customization using @TODO:
+# Example:
 # @TODO:
 #
 # Requires: wget, jq, fzf >= 0.29.0 (will be installed if not present)
@@ -18,7 +18,7 @@ set -Eeuo pipefail
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-# download latest fzf 
+# download latest fzf
 if ! (command -v "$script_dir/fzf" 1 > /dev/null); then
   (cd "$script_dir/.." && wget -qO- https://raw.githubusercontent.com/junegunn/fzf/master/install | $SHELL -s -- --bin)
 fi
@@ -27,7 +27,7 @@ help() {
   cat <<EOF
 Usage: $(basename "${BASH_SOURCE[0]}") -c <dir/executable> [options...]
 
-A terminal ui exposing commands and their markdown documentaion 
+A terminal ui exposing commands and their markdown documentaion
 
 Available options:
 
@@ -37,10 +37,10 @@ Available options:
 -pl, --preview-label             preview/documentation label text
 -bl, --border-label              shaunch label text
 -t,  --title                     Title string above the commands to launch
--c,  --commands <dir/executable> Directory to scan for commands (defaults to current directory) 
-                                 or executable returning json command structure  
+-c,  --commands <dir/executable> Directory to scan for commands (defaults to current directory)
+                                 or executable returning json command structure
 -- <command--returning-json>     everything behind '--' will be evaluated as bash script
-                                 the result is expected to return json command structure 
+                                 the result is expected to return json command structure
 EOF
   exit
 }
@@ -54,7 +54,7 @@ function scan_commands() {
     # append script only if matching markdown file exists
     if [[ -f "${dir}/${script}.md" ]]; then
       json=$(\
-        echo "$json" | 
+        echo "$json" |
         jq \
           --arg caption "$script" \
           --arg help "${dir}/${script}.md" \
@@ -84,7 +84,7 @@ function kambrium:render_markdown() {
   text=$(sed -E 's/(^|[^[:alnum:]_])~~([^_]+)~~([^[:alnum:]_]|$)/\1\\e[9m\2\\e[0m\3/g' <<< "$text")
 
   # highlight markdown links and pure links between '[...]()' underline "\e[4munderline\e[0m"
-  #  `(\[.*\])(\((http)(?:s)?(\:\/\/).*\))` 
+  #  `(\[.*\])(\((http)(?:s)?(\:\/\/).*\))`
   text=$(sed -E 's/(^|[^[:alnum:]_])(\[[^]]+\])?\((https?:[^)]+)\)([^[:alnum:]_]|$)/\1\\e[33m\2(\\e[0;4m\3\\e[33m)\\e[0m\4/g' <<< "$text")
 
   # highlight headings (#-#####) "\e[1bold\e[0m"
@@ -101,7 +101,7 @@ function render_markdown() {
     # if markdown file is a executable : execute it and interpret its output as markdown
     help=$($help)
   elif [[ -f "$help" ]]; then
-    # if its a regular markdown file 
+    # if its a regular markdown file
     help=$(cat $help)
   else
     # otherwise interpret content as markdown content
@@ -115,27 +115,27 @@ function render_markdown() {
 # executed on enter key from fzf
 function execute() {
   local command_caption="$1"
-  
-  # query "prompt" property for current command (fallback value "" returned if not found) 
+
+  # query "prompt" property for current command (fallback value "" returned if not found)
   local prompt=$(echo "$COMMANDS" | jq --arg caption "$command_caption" -r '.[] | select(.caption==$caption).prompt |  select(.!=null)')
   # if prompt is not empty : write to shaunch communication channel
-  if [[ "$prompt" != '' ]]; then 
+  if [[ "$prompt" != '' ]]; then
     printf "%s" "$prompt" > "$SHAUNCH_EXEC_FILE"
 
     $(printenv SHAUNCH_DOCKERIZED >/dev/null) && printf "
 Shaunch was runned from within a docker container : Cannot write to next prompt. Please execute
 
 '%s'
-      
+
 on next prompt.
 " "$prompt" >&2
   fi
 
-  # query "exec" property for current command (fallback value "" returned if not found) 
+  # query "exec" property for current command (fallback value "" returned if not found)
   local exec=$(echo "$COMMANDS" | jq --arg caption "$command_caption" -r '.[] | select(.caption==$caption).exec  | select(.!=null)')
-  if [[ "$exec" != '' ]]; then     
+  if [[ "$exec" != '' ]]; then
     # if exec is not empty execute it in a sub shell
-    bash -c "$exec"  
+    bash -c "$exec"
   elif [[ "$prompt" != '' ]]; then
     # if no exec but prompt trigger exit
     shaunch: exit
@@ -155,12 +155,12 @@ function shaunch:() {
     case "${1-}" in
       exit)
         # kill fzf
-        kill "$_shaunch_pid" 
+        kill "$_shaunch_pid"
         break;
       ;;
-      *) 
-        echo "Unknown shaunch command: $1" 
-        break 
+      *)
+        echo "Unknown shaunch command: $1"
+        break
       ;;
     esac
     shift
@@ -186,26 +186,26 @@ parse_params() {
       -h | --help)
         help
       ;;
-      -v | --verbose) 
-        set -x 
+      -v | --verbose)
+        set -x
       ;;
-      -t | --title) 
+      -t | --title)
         TITLE="${2-}"
         shift
       ;;
-      -pl | --preview-label) 
+      -pl | --preview-label)
         PREVIEW_LABEL="${2-}"
         shift
       ;;
-      -bl | --border-label) 
+      -bl | --border-label)
         BORDER_LABEL="${2-}"
         shift
       ;;
-      --) 
+      --)
         shift
         export COMMANDS=$($@)
       ;;
-      -c | --commands) 
+      -c | --commands)
         if [[ -d "${2-}" ]]; then
           export COMMANDS=$(scan_commands "${2-}")
         elif [[ -x "${2-}" ]]; then
@@ -216,11 +216,11 @@ parse_params() {
         fi
         shift
       ;;
-      -?*) 
-        die "Unknown option: $1" 
+      -?*)
+        die "Unknown option: $1"
       ;;
-      *) 
-        break 
+      *)
+        break
       ;;
     esac
     shift
@@ -248,8 +248,8 @@ function onExit() {
 
   SHAUNCH_EXEC=$(cat "$SHAUNCH_EXEC_FILE")
   rm -rf -- "$SHAUNCH_EXEC_FILE"
-  perl -e 'ioctl(STDIN, 0x5412, $_) for split "", join " ", @ARGV' "$SHAUNCH_EXEC" 
-  stty "$saved_settings" 
+  perl -e 'ioctl(STDIN, 0x5412, $_) for split "", join " ", @ARGV' "$SHAUNCH_EXEC"
+  stty "$saved_settings"
 }
 
 trap "onExit" EXIT
@@ -259,6 +259,7 @@ PREVIEW_CMD="'${BASH_SOURCE[0]}' render_markdown {}"
 # --select-1 \
 # see https://github.com/junegunn/fzf/issues/3089#issuecomment-1353158088 for the $PPID thingie
 cmd=$("$script_dir/fzf" \
+  --no-mouse \
   --reverse \
   --preview-window wrap \
   --border-label "$BORDER_LABEL" \
